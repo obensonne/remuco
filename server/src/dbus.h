e2a6cb5abee538fc47676fa77570cc63ec008d9b
Oben Sonne <obensonne@googlemail.com>
2008-05-11 21:40:57 +0000

    - error strings
    --HG--
    extra : convert_revision : df6f549fc454b353f7f08f816bef0350cdf512eb

--- server/src/dbus.h
+++ server/src/dbus.h
@@ -11,11 +11,11 @@
 
 #define REM_SERVER_ERR_DOMAIN			g_quark_from_string("rem_server_error")
 
-#define REM_SERVER_ERR_INVALID_DATA		"Conveyed data is invalid."
+#define REM_SERVER_ERR_INVALID_DATA		"conveyed data is invalid"
 #define REM_SERVER_ERR_INVALID_DATA_NUM	1
-#define REM_SERVER_ERR_VERSION			"Server version incompatible."
+#define REM_SERVER_ERR_VERSION			"incompatible server version"
 #define REM_SERVER_ERR_VERSION_NUM		2
-#define REM_SERVER_ERR_UNKNOWN			"Conveyed player name is unknown."
+#define REM_SERVER_ERR_UNKNOWN			"conveyed player name is unknown"
 #define REM_SERVER_ERR_UNKNOWN_NUM		3
 
 G_BEGIN_DECLS
