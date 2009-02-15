a93e73fef9ebb67a5b5c638ee85354f94d5d30ef
Oben Sonne <obensonne@googlemail.com>
2009-02-15 17:20:07 +0000

    - apply path model to ploblist
    --HG--
    extra : convert_revision : 8de395e7d88257426510db21fa1407a1ab7ee5d8

--- client/src/remuco/player/PlobList.java
+++ client/src/remuco/player/PlobList.java
@@ -5,108 +5,122 @@ import remuco.comm.SerialAtom;
 import remuco.util.Log;
 
 /**
- * A ploblist is a list of plobs. A ploblist does not contain real {@link Plob}s.
- * In fact for each contained plob, it stores the plob's PID and title (which
- * may be anything - however, using the plobs's title and artist as title is a
- * meaningful choice). The actually contained {@link Plob}s can be requested
- * via their P(L)IDs with {@link Player#reqPlob(String, IPlobRequestor)}.
+ * TODO doc
  * 
  * @author Oben Sonne
  * 
  */
 public final class PlobList implements ISerializable {
 
-	private static final String NAME_UNKNWON = "??NAME??";
+	protected static final String PATH_PLAYLIST[] = new String[] { "__PLAYLIST__" };
 
-	private static final String PLID_UNKNWON = "??PLID??";
+	protected static final String PATH_QUEUE[] = new String[] { "__QUEUE__" };
+
+	private static final String UNKNWON = "#~@X+.YO?/";
 
 	private final SerialAtom[] atoms;
 
-	private String name, id;
-
-	private String[] nestedIDs, nestedNames, plobIDs, plobNames;
+	private String pathStr, path[], nested[], plobIds[], plobNames[];
 
 	public PlobList() {
 
-		atoms = new SerialAtom[6];
-		atoms[0] = new SerialAtom(SerialAtom.TYPE_S);
-		atoms[1] = new SerialAtom(SerialAtom.TYPE_S);
+		atoms = new SerialAtom[4];
+		atoms[0] = new SerialAtom(SerialAtom.TYPE_AS);
+		atoms[1] = new SerialAtom(SerialAtom.TYPE_AS);
 		atoms[2] = new SerialAtom(SerialAtom.TYPE_AS);
 		atoms[3] = new SerialAtom(SerialAtom.TYPE_AS);
-		atoms[4] = new SerialAtom(SerialAtom.TYPE_AS);
-		atoms[5] = new SerialAtom(SerialAtom.TYPE_AS);
 
-		reset();
+		pathStr = null;
+		path = null;
+		nested = null;
+		plobIds = null;
+		plobNames = null;
 	}
 
 	public void atomsHasBeenUpdated() {
-
-		id = atoms[0].s;
-		name = atoms[1].s;
-
-		nestedIDs = new String[atoms[2].as.length];
-		System.arraycopy(atoms[2].as, 0, nestedIDs, 0, atoms[2].as.length);
-
-		nestedNames = new String[atoms[3].as.length];
-		System.arraycopy(atoms[3].as, 0, nestedNames, 0, atoms[3].as.length);
-
-		plobIDs = new String[atoms[4].as.length];
-		System.arraycopy(atoms[4].as, 0, plobIDs, 0, atoms[4].as.length);
-
-		plobNames = new String[atoms[5].as.length];
-		System.arraycopy(atoms[5].as, 0, plobNames, 0, atoms[5].as.length);
-
+		pathStr = null;
+		path = atoms[0].as;
+		nested = atoms[1].as;
+		plobIds = atoms[2].as;
+		plobNames = atoms[3].as;
 	}
 
 	public SerialAtom[] getAtoms() {
 		return atoms;
 	}
 
-	public String getID() {
-		return id;
-	}
-
 	public String getName() {
-		return name;
-	}
-
-	/** Get the ID of the nested ploblist #<code>i</code> (starting from 0). */
-	public String getNestedID(int i) {
-
-		try {
-			return nestedIDs[i];
-		} catch (ArrayIndexOutOfBoundsException e) {
-			return "#~@X+.YO?/";
+		if (path == null || path.length == 0) {
+			return "Library";
 		}
+		return path[path.length - 1];
 	}
 
 	/** Get the name of the nested ploblist #<code>i</code> (starting from 0). */
-	public String getNestedName(int i) {
+	public String getNested(int i) {
 
 		try {
-			return nestedNames[i];
+			return nested[i];
+		} catch (NullPointerException e) {
+			return UNKNWON;
 		} catch (ArrayIndexOutOfBoundsException e) {
-			return "#~@X+.YO?/";
+			return UNKNWON;
 		}
 	}
 
 	public int getNumNested() {
-		
-		return nestedIDs.length;
+
+		return nested != null ? nested.length : 0;
 	}
 
 	public int getNumPlobs() {
-		
-		return plobIDs.length;
+
+		return plobIds != null ? plobIds.length : 0;
+	}
+
+	/** Get the ploblist's path as slash separated list. */
+	public String getPath() {
+
+		if (pathStr == null) {
+
+			if (path == null || path.length == 0) {
+				pathStr = "";
+			}
+			final StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < path.length; i++) {
+				sb.append(path[i]).append('/');
+			}
+			sb.deleteCharAt(sb.length() - 1);
+			pathStr = sb.toString();
+			
+		}
+		return pathStr;
+	}
+
+	public String getPathForNested(int i) {
+
+		if (i >= nested.length) {
+			return UNKNWON;
+		}
+
+		final String base = getPath();
+		if (base.length() == 0) {
+			return nested[i];
+		} else {
+			return base + "/" + nested[i];
+		}
+
 	}
 
 	/** Get the PID of the plob #<code>i</code> (starting from 0). */
 	public String getPlobID(int i) {
 
 		try {
-			return plobIDs[i];
+			return plobIds[i];
+		} catch (NullPointerException e) {
+			return UNKNWON;
 		} catch (ArrayIndexOutOfBoundsException e) {
-			return "#~@X+.YO?/";
+			return UNKNWON;
 		}
 	}
 
@@ -115,34 +129,23 @@ public final class PlobList implements ISerializable {
 
 		try {
 			return plobNames[i];
+		} catch (NullPointerException e) {
+			return UNKNWON;
 		} catch (ArrayIndexOutOfBoundsException e) {
-			return "#~@X+.YO?/";
+			return UNKNWON;
 		}
 	}
 
 	public String toString() {
 
-		StringBuffer sb = new StringBuffer("Ploblist: ").append(id);
-		sb.append(" (").append(name).append(")\n");
-
-		return sb.toString();
+		return "Ploblist: '" + getPath() + "'";
 	}
 
 	public void updateAtoms() {
 
 		// not needed
-		Log.asssertNotReached(this);
-	}
+		Log.bug("Feb 6, 2009.12:57:27 AM");
 
-	protected void reset() {
-
-		name = NAME_UNKNWON;
-		id = PLID_UNKNWON;
-
-		nestedIDs = new String[0];
-		nestedNames = new String[0];
-		plobIDs = new String[0];
-		plobNames = new String[0];
 	}
 
 }
