0709d1a04d1d6bab953b72f2d49cadeef743fde5
Oben Sonne <obensonne@googlemail.com>
2009-02-16 19:41:35 +0000

    - removed obsolete keys
    --HG--
    extra : convert_revision : edb5462927575797094c0b51359a491fd71a10e6

--- client/src/remuco/ui/Keys.java
+++ client/src/remuco/ui/Keys.java
@@ -19,35 +19,26 @@ public final class Keys {
 	public static final int ACTION_NOOP = 100;
 
 	/** Action code. */
-	public static final int ACTION_PLAYPAUSE = 0, ACTION_STOP = 1,
-			ACTION_NEXT = 2, ACTION_PREV = 3, ACTION_VOLUP = 4,
-			ACTION_VOLDOWN = 5, ACTION_VOLMUTE = 6, ACTION_RATEUP = 7,
-			ACTION_RATEDOWN = 8, ACTION_EDITTAGS = 9, ACTION_PLAYLIST = 10,
-			ACTION_IMAGE = 11, ACTION_COUNT = 13;
-
-	/**
-	 * Action code.
-	 * 
-	 * @deprecated
-	 */
-	public static final int ACTION_LIBRARY = 12;
+	public static final int ACTION_PLAYPAUSE = 0, ACTION_NEXT = 1,
+			ACTION_PREV = 2, ACTION_VOLUP = 3, ACTION_VOLDOWN = 4,
+			ACTION_VOLMUTE = 5, ACTION_RATEUP = 6, ACTION_RATEDOWN = 7,
+			ACTION_EDITTAGS = 8, ACTION_IMAGE = 9, ACTION_COUNT = 10;
 
 	/**
 	 * Mapping of action codes to their names.
 	 */
 	public static final String[] actionNames = new String[] { "Play/Pause",
-			"Stop", "Next", "Previous", "Volume up", "Volume down",
-			"Volume mute", "Rate up", "Rate down", "Edit Tags",
-			"Show playlist", "Show image", "Show Library" };
+			"Next", "Previous", "Volume up", "Volume down", "Volume mute",
+			"Rate up", "Rate down", "Edit Tags", "Show image" };
 
 	/**
 	 * Key configuration. Format described at {@link #defaultConfig}. Initially
 	 * it is the same as {@link #defaultConfig}.
 	 */
-	private static final int[] config = new int[] { Canvas.KEY_NUM5, 0,
+	private static final int[] config = new int[] { Canvas.KEY_NUM5,
 			Canvas.KEY_NUM8, Canvas.KEY_NUM2, Canvas.KEY_NUM6, Canvas.KEY_NUM4,
 			Canvas.KEY_NUM1, Canvas.KEY_POUND, Canvas.KEY_STAR,
-			Canvas.KEY_NUM3, Canvas.KEY_NUM7, Canvas.KEY_NUM0, Canvas.KEY_NUM9 };
+			Canvas.KEY_NUM3, Canvas.KEY_NUM0 };
 
 	/**
 	 * Default key configuration.
@@ -56,10 +47,10 @@ public final class Keys {
 	 * The key code at <code>i</code> is mapped to the action code
 	 * <code>i</code>.
 	 */
-	private static final int[] defaultConfig = new int[] { Canvas.KEY_NUM5, 0,
+	private static final int[] defaultConfig = new int[] { Canvas.KEY_NUM5,
 			Canvas.KEY_NUM8, Canvas.KEY_NUM2, Canvas.KEY_NUM6, Canvas.KEY_NUM4,
 			Canvas.KEY_NUM1, Canvas.KEY_POUND, Canvas.KEY_STAR,
-			Canvas.KEY_NUM3, Canvas.KEY_NUM7, Canvas.KEY_NUM0, Canvas.KEY_NUM9 };
+			Canvas.KEY_NUM3, Canvas.KEY_NUM0 };
 
 	/**
 	 * Set a new key configuration.
@@ -73,7 +64,7 @@ public final class Keys {
 	public static boolean configure(int[] newConfig) {
 
 		if (!check(newConfig)) {
-			Log.ln("[KECO] bad config, keep current");
+			Log.ln("[KE] bad config, keep current");
 			return false;
 		}
 
@@ -222,12 +213,12 @@ public final class Keys {
 	private static boolean check(int[] config) {
 
 		if (config == null) {
-			Log.ln("[KECO] check: null config");
+			Log.ln("[KE] check: null config");
 			return false;
 		}
 
 		if (config.length != ACTION_COUNT) {
-			Log.ln("[KECO] check: wrong mapping length");
+			Log.ln("[KE] check: wrong mapping length");
 			return false;
 		}
 
@@ -236,7 +227,7 @@ public final class Keys {
 				continue;
 			for (int j = i + 1; j < config.length; j++) {
 				if (config[i] == config[j]) {
-					Log.ln("[KECO] check: key " + config[i] + " is set twice");
+					Log.ln("[KE] check: key " + config[i] + " is set twice");
 					return false;
 				}
 			}
