d3fbc9e5dc41e2f13fcf9443038b2969b5aec4d5
Oben Sonne <obensonne@googlemail.com>
2009-02-16 19:33:18 +0000

    - doc update
    --HG--
    extra : convert_revision : 4bd5cf5de41297828cde6982e799b81dafe841b1

--- client/src/remuco/player/IPlobRequestor.java
+++ client/src/remuco/player/IPlobRequestor.java
@@ -3,14 +3,11 @@ package remuco.player;
 public interface IPlobRequestor {
 
 	/**
-	 * Response to a previously requested plob. The plob is the same used as an
-	 * argument when requesting the plob with
-	 * {@link Player#reqPlob(String, Plob, IPlobRequestor)} but with updated
-	 * content.
+	 * Response to a previously requested plob.
 	 * 
-	 * @param p
-	 *            the previously requested plob
+	 * @param plob
+	 *            the requested plob
 	 */
-	public void handlePlob(Plob p);
+	public void handlePlob(Plob plob);
 
 }
--- client/src/remuco/player/IPloblistRequestor.java
+++ client/src/remuco/player/IPloblistRequestor.java
@@ -1,22 +1,12 @@
 package remuco.player;
 
-/**
- * For classes, which listen for {@link PlobList}s other than playlist or
- * queue.
- * 
- * @author Oben Sonne
- * 
- */
 public interface IPloblistRequestor {
 
 	/**
-	 * Response to a previously requested ploblist. The ploblist is the same
-	 * used as an argument when requesting the ploblist with
-	 * {@link Player#reqPloblist(String, PlobList, IPloblistRequestor)} but with
-	 * updated content.
+	 * Response to a previously requested ploblist.
 	 * 
-	 * @param p
-	 *            the previously requested ploblist
+	 * @param ploblist
+	 *            the requested ploblist
 	 */
 	public void handlePloblist(PlobList ploblist);
 
