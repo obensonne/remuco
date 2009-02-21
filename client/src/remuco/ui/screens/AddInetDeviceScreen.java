87c83873bdc4c81f3eee4f1cd12501dd08fffcdf
Oben Sonne <obensonne@googlemail.com>
2009-02-21 14:43:07 +0000

    - add options field - adjust textfield restrictions
    --HG--
    extra : convert_revision : 3b487b6bb696e411182ca98542544d5896ae11b2

--- client/src/remuco/ui/screens/AddInetDeviceScreen.java
+++ client/src/remuco/ui/screens/AddInetDeviceScreen.java
@@ -7,38 +7,57 @@ import remuco.comm.InetServiceFinder;
 
 public class AddInetDeviceScreen extends Form {
 
-	private final TextField tfHost, tfPort;
+	private final TextField tfHost, tfPort, tfOptions;
 
 	public AddInetDeviceScreen() {
 
 		super("WiFi Connection");
 
-		tfHost = new TextField("Host or IP address:", "", 256, TextField.NON_PREDICTIVE);
+		tfHost = new TextField("Host or IP address:", "", 256,
+				TextField.URL);
 
-		tfPort = new TextField("Port number (if unsure, do not change):", "" + InetServiceFinder.PORT, 5, TextField.URL);
+		tfPort = new TextField("Port number (if unsure, do not change):", ""
+				+ InetServiceFinder.PORT, 5, TextField.NUMERIC);
 
-		//append(new CenteredImageItem(Theme.ICON_WIFI_20));
-		append(" ");
+		tfOptions = new TextField("Options (if unsure, leave empty):", "", 256,
+				TextField.NON_PREDICTIVE);
+
+		//append(" ");
 		append(tfHost);
-		append(" ");
+		//append(" ");
 		append(tfPort);
+		//append(" ");
+		append(tfOptions);
 	}
-	
+
 	public String getAddress() {
-		
+
 		final String host = tfHost.getString();
 		final String port = tfPort.getString();
+		final String options = tfOptions.getString();
 		
 		if (host.length() == 0) {
 			return null;
 		}
+
+		final StringBuffer sb = new StringBuffer(host);
+
+		sb.append(':');
 		
 		if (port.length() == 0) {
-			return host + ":" + InetServiceFinder.PORT;
+			sb.append(InetServiceFinder.PORT);
 		} else {
-			return host + ":" + port;			
+			sb.append(port);
 		}
 		
+		if (options.length() > 0) {
+			if (!options.startsWith(";")) {
+				sb.append(';');
+			}
+			sb.append(options);
+		}
+
+		return sb.toString();
 	}
 
 }
