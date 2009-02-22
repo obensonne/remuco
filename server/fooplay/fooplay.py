1567650b5bad6af89665e02b02df2829bdaa6007
Oben Sonne <obensonne@googlemail.com>
2009-02-22 23:32:24 +0000

    - example doc - signal handling
    --HG--
    extra : convert_revision : cb641a3467e56b922bbdc3a9e83e409e3de5aaa7

--- server/fooplay/fooplay.py
+++ server/fooplay/fooplay.py
@@ -1,48 +1,47 @@
+#!/usr/bin/python
+
 import sys
 import gobject
 import dircache
+import signal
 
 try:
     import remuco
+    from remuco import log
     IMPORT_ERROR = None
 except ImportError, e:
     IMPORT_ERROR = str(e)
 
-from remuco import log
-
 class FooPlay(remuco.Player):
     
     def __init__(self):
+        
+        # important: this must be called first:
         remuco.Player.__init__(self, "FooPlay")
-    
+        
+        # example: periodic player state update function:
+        sid = gobject.timeout_add(5000, self.__update_state)
+        
+        # important: remember source ids (for shutting down):
+        self.__gobject_source_ids = (id,)
+        
+        # example: logging
+        log.debug("FooPlay.__init__() done")
+        
     def request_playlist(self, client):
-        """ Request the content of the currently active playlist. 
         
-        @param client: the requesting client (needed for reply)
-        
-        @note: This method should be overwritten by sub classes of Player. It
-               gets called if a remote client requests a PLOB from the player.
-
-        @see: reply_playlist_request() for sending back the result
-        """
-        log.debug("request_playlist()")
-        self.reply_playlist_request(client, ["1", "2"], ["X - X", "AA - BB"])
+        # example: 2 tracks in the playlist
+        self.reply_playlist_request(client, ["1", "2"],
+                                    ["Joe - Joe's Song", "Sue - Sue's Song"])
 
     def request_queue(self, client):
-        """ Request the content of the play queue. 
         
-        @param client: the requesting client (needed for reply)
-        
-        @note: This method should be overwritten by sub classes of Player. It
-               gets called if a remote client requests a PLOB from the player.
-
-        @see: reply_queue_request() for sending back the result
-        """
-        log.debug("request_queue()")
+        # example: empty queue
         self.reply_queue_request(client, [], [])
 
     def request_library(self, client, path):
-        log.debug("request_library()")
+        
+        # example: show file system as media player library
         
         nested = []
         plob_ids = []
@@ -50,7 +49,7 @@ class FooPlay(remuco.Player):
 
         dir = "/"
         for elem in path:
-            dir = dir + elem + "/"
+            dir += elem + "/"
             
         try:
             list = dircache.listdir(dir)
@@ -70,23 +69,70 @@ class FooPlay(remuco.Player):
                 plob_names.append(elem)
         
         self.reply_library_request(client, path, nested, plob_ids, plob_names)
+        
+    def down(self):
+        
+        # important: call super class implementation:
+        remuco.Player.down(self)
+        
+        # example: remove sources we've added to the gobject main loop:
+        for source_id in self.__gobject_source_ids:
+            gobject.source_remove(source_id)
 
-def fn(player):
-    player.update_volume(23)
-    return True
+    def __update_state(self):
+        
+        # example: update current volume
+        self.update_volume(23)
+        
+        return True
+
+# =============================================================================
+# main (example startup)
+# =============================================================================
+
+ml = None
+
+def sighandler(signum, frame):
+    
+    global ml
+    
+    log.info("received signal %i" % signum)
+    
+    if ml != None:
+        ml.quit()
+        
+def main():
+    
+    signal.signal(signal.SIGINT, sighandler)
+    signal.signal(signal.SIGTERM, sighandler)
+
+    try:
+        fp = FooPlay()
+    except Exception, e:
+        print("Failed to set up FooPlay player adapter: %s" % str(e))
+        print("See remuco-fooplay log file for details.")
+        return
+    
+    global ml
+    
+    ml = gobject.MainLoop()
+    
+    try:
+        ml.run() # Remuco always needs a main loop to run
+    except Exception, e:
+        print("exception in main loop: %s" % str(e))
+    
+    # main loop has been stopped
+    
+    fp.down()
+
+# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     
     if IMPORT_ERROR != None:
-        log.error("missing python module (%s)" % IMPORT_ERROR)
+        log.error("missing a python module (%s)" % IMPORT_ERROR)
     else:
         log.info("starting")
         
-        p = FooPlay()
-        
-        ml = gobject.MainLoop()
-        
-        gobject.timeout_add(20000, fn, p)
-        
-        ml.run()
-    
+    main()
