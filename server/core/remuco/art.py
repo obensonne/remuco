40b853c8f61820e5c7f8ecb0e1d774b496cd22e3
Oben Sonne <obensonne@googlemail.com>
2009-03-01 21:33:42 +0000

    - refactoring - debug log
    --HG--
    extra : convert_revision : c2af3b7a2512924a6683a329462e8c8f67968c3a

--- server/core/remuco/art.py
+++ server/core/remuco/art.py
@@ -12,37 +12,6 @@ TN_SUBDIRS = ("large", "normal")
 ART_FILE_NAMES = ("folder", "front", "album", "cover", "art")
 ART_FILE_TYPES = ("png", "jpeg", "jpg")
 
-def __get_art_in_folder(resource):
-    """Try to find art images in the given resource' folder.
-    
-    @param resource: a local path or an URL (string)
-    
-    @return: path to an image file or None if resource is not local or if there
-             is no matching image file in the resource' folder
-    """
-    
-    elems = urlparse.urlparse(resource)
-    
-    if elems[0] is not None and elems[0] != "file":
-        return None
-    
-    path = urllib.url2pathname(elems[2])
-    path = os.path.dirname(path)
-    
-    log.debug("looking for an image in %s" % path)
-    
-    for name in ART_FILE_NAMES:
-        for type in ART_FILE_TYPES:
-            for ext in (type, type.upper()):
-                file = os.path.join(path, "%s.%s" % (name, ext))
-                if os.path.isfile(file):
-                    return file
-                file = os.path.join(path, "%s.%s" % (name.capitalize(), ext))
-                if os.path.isfile(file):
-                    return file
-            
-    return None
-
 def __resource_to_file_uri(resource):
     """Convert a resource to a file URI (file://...).
     
@@ -68,7 +37,35 @@ def __resource_to_file_uri(resource):
     
     return None
 
-def __get_art_from_thumbnails(resource):
+def __get_art_in_folder(uri):
+    """Try to find art images in the given resource' folder.
+    
+    @param resource: a file uri ('file://...')
+    
+    @return: path to an image file or None if there is no matching image file
+             in the resource' folder
+    """
+    
+    elems = urlparse.urlparse(uri)
+    
+    path = urllib.url2pathname(elems[2])
+    path = os.path.dirname(path)
+    
+    log.debug("looking for art image in %s" % path)
+    
+    for name in ART_FILE_NAMES:
+        for type in ART_FILE_TYPES:
+            for ext in (type, type.upper()):
+                file = os.path.join(path, "%s.%s" % (name, ext))
+                if os.path.isfile(file):
+                    return file
+                file = os.path.join(path, "%s.%s" % (name.capitalize(), ext))
+                if os.path.isfile(file):
+                    return file
+            
+    return None
+
+def __get_art_from_thumbnails(uri):
     """Try to find a thumbnail for the given resource.
     
     @param resource: a local path or an URL (string)
@@ -80,20 +77,14 @@ def __get_art_from_thumbnails(resource):
     if not os.path.isdir(TN_DIR):
         return None
     
-    uri = __resource_to_file_uri(resource)
-    
-    if uri is None:
-        log.debug("%s is not local, cannot get thumbnail" % resource)
-        return None
-    
+    log.debug("looking for art image in %s" % TN_DIR)
+
     hex = md5.new(uri).hexdigest()
     
     for subdir in TN_SUBDIRS:
         file = os.path.join(TN_DIR, subdir, "%s.png" % hex)
         if os.path.isfile(file):
             return file
-    
-    log.debug("no thumbnail for %s" % resource)
     
     return None
 
@@ -102,17 +93,22 @@ def get_art(resource, prefer_thumbnail=False):
     if resource is None:
         return None
     
+    uri = __resource_to_file_uri(resource)
+    if uri is None:
+        log.debug("resource '%s' is not local, ignore" % resource)
+        return None
+    
     if prefer_thumbnail:
-        file = __get_art_from_thumbnails(resource)
+        file = __get_art_from_thumbnails(uri)
         if file is not None:
             return file
         
-    file = __get_art_in_folder(resource)
+    file = __get_art_in_folder(uri)
     if file is not None:
         return file
     
     if not prefer_thumbnail:
-        file = __get_art_from_thumbnails(resource)
+        file = __get_art_from_thumbnails(uri)
         if file is not None:
             return file
     
