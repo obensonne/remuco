fe29011354699373a18548bad39ccc5c2ea6b07f
Oben Sonne <obensonne@googlemail.com>
2009-03-20 23:17:55 +0000

    - module check in make file
    --HG--
    extra : convert_revision : 1a59964ce22ff9081a08579b14e2116fc4f6c857

--- server/core/setup.py
+++ server/core/setup.py
@@ -1,26 +1,5 @@
 from distutils.core import setup
 
-import sys
-
-
-if len(sys.argv) > 1 and "install" in sys.argv:
-    
-    # check for required modules
-    try:
-        import gobject
-        import xdg.BaseDirectory
-        import Image
-        import logging
-        import bluetooth
-        import dbus
-    except ImportError, e:
-        print("Missing a required python module: %s" % str(e))
-        sys.exit(1)
-        
-    # ensure installed files get logged:
-    sys.argv.append("--record")
-    sys.argv.append("install.log")
-
 setup(name='remuco',
       version='0.8.0',
       description='Module for Remuco player adapters.',
