7188b88e80d62051a44c40b9d94ece45b689f330
Oben Sonne <obensonne@googlemail.com>
2009-03-28 23:35:46 +0000

    - add client-only tarball script
    --HG--
    extra : convert_revision : 8aaa788ae0e1c92c9b9e14bbd3ed50b53623a954

---
+++ tarball-client.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+VERSION=$1
+
+[ -z "$VERSION" ] && echo "need version" && exit 1
+
+# -----------------------------------------------------------------------------
+# preparations
+# -----------------------------------------------------------------------------
+
+PKG=remuco-client-$VERSION
+
+rm -rf $PKG $PKG.tar.gz || exit 1
+
+mkdir $PKG
+
+# -----------------------------------------------------------------------------
+# client
+# -----------------------------------------------------------------------------
+
+cd client
+
+ant dist
+
+cp -r app/* ../$PKG/
+
+cd ..
+
+cat > $PKG/README << EOF
+Remuco client binaries.
+
+Please visit http://remuco.sourceforge.net/index.php/Getting_Started for
+installation and usage instructions.
+EOF
+
+# -----------------------------------------------------------------------------
+# clean up and package
+# -----------------------------------------------------------------------------
+
+find $PKG -type d -name ".svn" | xargs rm -rf
+find $PKG -type f -name "*~" | xargs rm -f
+
+
+tar zcf $PKG.tar.gz $PKG
+
+rm -rf $PKG
+
