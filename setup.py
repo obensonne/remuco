8e92ce17c776399bca484940d73dc3ffa865445c
Oben Sonne <obensonne@googlemail.com>
2009-09-30 15:07:20 +0200

    Sort adapters alphabetically

--- setup.py
+++ setup.py
@@ -60,6 +60,12 @@ pa_files["mpd"] = [
     ["remuco-mpd"]
 ]
 
+# --- Okular ------------------------------------------------------------------
+
+pa_files["okular"] = [
+    ["remuco-okular"]
+]
+
 # --- Rhythmbox ---------------------------------------------------------------
 
 # set prefix may not be valid for Rhythmbox -> use a Rhythmbox specific prefix:
@@ -98,12 +104,6 @@ pa_files["vlc"] = [
 
 pa_files["xmms2"] = [
     ["remuco-xmms2"]
-]
-
-# --- Okular ------------------------------------------------------------------
-
-pa_files["okular"] = [
-    ["remuco-okular"]
 ]
 
 # =============================================================================
