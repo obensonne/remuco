498e456725ee1bf4ede2ad294863e931ec3b73f2
Oben Sonne <obensonne@googlemail.com>
2009-11-12 00:31:46 +0100

    Call totem subprocess asynchronously
    Totem 2.28 seems to run the commandline tool for controlling an
    already running Totem instance synchronously, which causes a dead lock
    when the main Totem instance itself runs 'totem' (as done by the Remuco
    plugin). Now the call has been implemented asynchronously in the Totem
    Remuco plugin to prevent this lock. A drawback is that errors during the
    call to 'totem' do not get catched but end up in standard out.
    
    Fixes issue 62

--- adapter/totem/remotem.py
+++ adapter/totem/remotem.py
@@ -211,13 +211,9 @@ class TotemAdapter(remuco.PlayerAdapter):
     def action_files(self, action_id, files, uris):
         
         if action_id == FA_ENQUEUE.id:
-            ret = subprocess.call(["totem", "--enqueue"] + uris)
-            if ret != os.EX_OK:
-                log.warning("failed to enqueue URIs to playlist")
+            subprocess.Popen(["totem", "--enqueue"] + uris)
         elif action_id == FA_SETPL.id:
-            ret = subprocess.call(["totem", "--replace"] + uris)
-            if ret != os.EX_OK:
-                log.warning("failed to fill playlist with URIs")
+            subprocess.Popen(["totem", "--replace"] + uris)
         else:
             log.error("** BUG ** unexpected action ID")
 
