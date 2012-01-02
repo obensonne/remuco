86f597f19e10aa469d39c2ea116d2a6ba4151224
Oben Sonne <obensonne@googlemail.com>
2012-01-02 21:57:24 +0100

    All: update version number to 0.9.6

--- setup.py
+++ setup.py
@@ -242,7 +242,7 @@ if client and os.path.exists("%s/remuco.apk" % sdir):
 # =============================================================================
 
 setup(name='remuco',
-      version='0.9.5',
+      version='0.9.6',
       description='Remuco is a remote control system for media players.',
       author='Remuco team',
       url='http://remuco.googlecode.com',
